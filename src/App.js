070fc4ad8ae9b87d65a0492342a9024da693ba6e
Maria Chira <geaguleamaria@yahoo.com>
2021-10-06 20:55:21 +0300

    add logo, style header, search input and button

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
-import Header from './components/header';
-import './App.css';
-import React from 'react';
+import Header from "./components/header"
+import "./App.css"
+import React, { useState } from "react"
 
-const rendomString = "Blabla";
+const rendomString = "Blabla"
 
 function App() {
+  const [searchStr, setSearchStr] = useState('');
 
   function handleHeaderOnClickBtn(btnState) {
     console.log("click-from-header", btnState)
   }
 
   function generateList(length) {
-    const arr = [];
-    for(let i=0; i<length; i++){
-      arr.push(`item-${i}`);
+    const arr = []
+    for (let i = 0; i < length; i++) {
+      arr.push(`item-${i}`)
     }
-    return arr;
+    return arr
   }
+
+  function handleSearchStr(e) {
+    console.log(e.target.value);
+    setSearchStr(e.target.value);
+  }
+
+  function startSearch() {
+    console.log("search")
+  }
+
 
   return (
     <div className="App">
-      <Header listItems={generateList(100)} onClickBtn={handleHeaderOnClickBtn} />
-      <Header listItems={[1, 2, 3]} />
-      <Header listItems={[]} />
-      <Header />
-      {/* <h2>{rendomString}</h2> */}
+      <Header
+        listItems={generateList(100)}
+        onClickBtn={handleHeaderOnClickBtn}
+        handleSearchInput={handleSearchStr}
+        handleButtonSearch={startSearch}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import Logo from './logo/Logo';
 import './header.css';
+
+
+/*
+1. Buton de login
+2. Icon Brand
+3. Search Field
+4. Help link
+*/
 
 class Header extends React.Component {
     constructor() {
@@ -36,19 +45,27 @@ class Header extends React.Component {
     render() {
 
         return (
-            <React.Fragment>
-                <h1>{this.state.buttonState ? "Button True" : "Button false"}</h1>
-                <button
-                    className={this.state.buttonState ? "header-btn-style" : "header-btn-style-select"}
-                    onClick={this.handleBtnOnClick}>Change-Title</button>
-                {(this.props.listItems && this.props.listItems.length > 0) &&
-                <ul>
-                    {this.props.listItems.map(elem => (
-                        <li>{elem}</li>
-                    ))}
-                </ul>
-                }
-            </React.Fragment>
+            // <React.Fragment>
+            //     <h1>{this.state.buttonState ? "Button True" : "Button false"}</h1>
+            //     <button
+            //         className={this.state.buttonState ? "header-btn-style" : "header-btn-style-select"}
+            //         onClick={this.handleBtnOnClick}>Change-Title</button>
+            //     {(this.props.listItems && this.props.listItems.length > 0) &&
+            //     <ul>
+            //         {this.props.listItems.map(elem => (
+            //             <li>{elem}</li>
+            //         ))}
+            //     </ul>
+            //     }
+            // </React.Fragment>
+            <div className="header-container">
+                <Logo width="4rem"/>
+                <div>
+                    <input type="text" onChange={this.props.handleSearchInput} />
+                    <button onClick={this.props.handleButtonSearch}>Search</button>   
+                </div>
+               
+            </div>
         )
     }
 }
